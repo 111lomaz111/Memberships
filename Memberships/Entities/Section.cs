5c16b5ebe06f38270a520dfd7f967c04a9d4aad0
Cezary Łysoń <LoMaZ111@poczta.onet.pl>
2019-02-17 20:04:47 +0100

    added itemtype, section, and part models and pushed them into DB

---
+++ Memberships/Entities/Section.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Entities
+{
+    [Table("Section")]
+    public class Section
+    {
+        /// <summary>
+        /// Generating auto incresing value in DB 
+        /// </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Setting max length for this prop
+        /// Setting something like not null in sql(?) for this prop
+        /// </summary>
+        [MaxLength(255)]
+        [Required]
+        public string Title { get; set; }
+    }
+}
