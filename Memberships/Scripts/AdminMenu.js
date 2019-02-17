cd87270201fe05d877d90f6055dc81d545cafbad
Cezary Łysoń <LoMaZ111@poczta.onet.pl>
2019-02-17 18:56:40 +0100

    added partial view and area for admin menu with own options, js,

---
+++ Memberships/Scripts/AdminMenu.js
@@ -0,0 +1,6 @@
+﻿$(function () {
+    $('[data-admin-menu]').hover(function () {
+        //adding/removing class to/from element with attribut 'data-admin-menu'
+        $('[data-admin-menu]').toggleClass('open');
+    });
+}); 
