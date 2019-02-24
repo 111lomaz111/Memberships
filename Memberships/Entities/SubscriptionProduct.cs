b7c2e0fcfe770c2b7472243190214c3ef5ba24de
Cezary Łysoń <LoMaZ111@poczta.onet.pl>
2019-02-24 16:00:21 +0100

    added models: productItem, ProductLinkText, ProductType, Subscription, SubscriptionProduct, UserSubscription

---
+++ Memberships/Entities/SubscriptionProduct.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Memberships.Entities
+{
+    [Table("SubscriptionProduct")]
+    public class SubscriptionProduct
+    {
+        [Required]
+        [Key, Column(Order = 1)]
+        public int ProductId{ get; set; }
+        [Required]
+        [Key, Column(Order = 2)]
+        public int SubscriptionId { get; set; }
+        [NotMapped]//entity will not create this columns in DB
+        public int OldProductId { get; set; }
+        [NotMapped] 
+        public int OldSubscriptionId { get; set; }
+    }
+}
