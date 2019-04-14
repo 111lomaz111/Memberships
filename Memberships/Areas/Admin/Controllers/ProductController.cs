43687860c574fed9351aad69ccf455dc3560209e
Cezary Łysoń <LoMaZ111@poczta.onet.pl>
2019-04-14 20:13:19 +0200

    -created subscription controller with CRUD methods -created product controller with CRUD methods -fixed 'edit' button text -added new method to conversion

---
+++ Memberships/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Memberships.Entities;
+using Memberships.Models;
+using Memberships.Areas.Admin.Extensions;
+using Memberships.Areas.Admin.Models;
+
+namespace Memberships.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _dbContext = new ApplicationDbContext();
+
+        // GET: Admin/Product
+        public async Task<ActionResult> Index()
+        {
+            var products = await _dbContext.Products.ToListAsync();
+            var model = await products.Convert(_dbContext);
+
+            return View(model);
+        }
+
+        // GET: Admin/Product/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = await _dbContext.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            var model = await product.Convert(_dbContext);
+            return View(model);
+        }
+
+        // GET: Admin/Product/Create
+        public async Task<ActionResult> Create()
+        {
+            var model = new ProductModel
+            {
+                ProductLinkTexts = await _dbContext.ProductLinkTexts.ToListAsync(),
+                ProductTypes = await _dbContext.ProductTypes.ToListAsync(),
+            };
+            return View(model);
+        }
+
+        // POST: Admin/Product/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Title,Description,ImageUrl,ProductLinkTextId,ProductTypeId")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Products.Add(product);
+                await _dbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // GET: Admin/Product/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = await _dbContext.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            var prod = new List<Product>();
+            prod.Add(product);
+            var ProductModel = await prod.Convert(_dbContext);
+            return View(ProductModel.First());
+        }
+
+        // POST: Admin/Product/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Title,Description,ImageUrl,ProductLinkTextId,ProductTypeId")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Entry(product).State = EntityState.Modified;
+                await _dbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Admin/Product/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = await _dbContext.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            var model = await product.Convert(_dbContext);
+            return View(model);
+        }
+
+        // POST: Admin/Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Product product = await _dbContext.Products.FindAsync(id);
+            _dbContext.Products.Remove(product);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _dbContext.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Memberships/Areas/Admin/Extensions/ConversionExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Memberships.Areas.Admin.Models;
+using Memberships.Entities;
+using Memberships.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Memberships.Areas.Admin.Extensions
+{
+    public static class ConversionExtensions
+    {
+        public static async Task<IEnumerable<ProductModel>> Convert(this IEnumerable<Product> products, ApplicationDbContext dbContext)
+        {
+            if (products.Count() == 0)
+                return new List<ProductModel>();
+
+            var text = await dbContext.ProductLinkTexts.ToListAsync();
+            var types = await dbContext.ProductTypes.ToListAsync();
+
+            return from p in products
+                   select new ProductModel
+                   {
+                       Id = p.Id,
+                       Title = p.Title,
+                       Description = p.Description,
+                       ImageUrl = p.ImageUrl,
+                       ProductLinkTextId = p.ProductLinkTextId,
+                       ProductLinkTexts = text,
+                       ProductTypeId = p.ProductTypeId,
+                       ProductTypes = types
+                   };
+        }
+
+        public static async Task<ProductModel> Convert(this Product product, ApplicationDbContext dbContext)
+        {
+            var productLinkText = await dbContext.ProductLinkTexts.FirstOrDefaultAsync(p => p.Id == product.ProductLinkTextId);
+            var productType = await dbContext.ProductTypes.FirstOrDefaultAsync(p => p.Id == product.ProductTypeId);
+
+            var model = new ProductModel
+                   {
+                       Id = product.Id,
+                       Title = product.Title,
+                       Description = product.Description,
+                       ImageUrl = product.ImageUrl,
+                       ProductLinkTextId = product.ProductLinkTextId,
+                       ProductLinkTexts = new List<ProductLinkText>(),
+                       ProductTypeId = product.ProductTypeId,
+                       ProductTypes = new List<ProductType>()
+            };
+
+            model.ProductLinkTexts.Add(productLinkText);
+            model.ProductTypes.Add(productType);
+
+            return model;
+        }
+
+    }
+}
